3489824eb298b5a71c05115667d2294384963dec
Aiman Rosli <aimanrosli977@yahoo.com>
2024-02-17 17:03:00 +0800

    Learn until 4.acceptuser input
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

---
+++ src/website1/index.js
@@ -0,0 +1,48 @@
+//console.log(`Hello`);
+//This is to show window alert
+//window.alert(`This is an alert`)
+
+//This is a comment
+
+//document.getElementById("myH1").textContent = `Hello`;
+//document.getElementById("myP").textContent = `I like pizza!`;
+
+//variable = A container that stores a value.
+
+// let x;
+// x = 123;
+// console.log(x);
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+
+//if want to check of datatype
+// console.log(typeof gpa);
+// console.log(`You are ${age} years old`);
+// console.log(`The price is $${price}`)
+// console.log(`Your gpa is : ${gpa}`)
+
+//Arithematic operators (+ - * /)
+//Example 
+//let students = 30;
+//students = students + 1;
+//students += students;
+//students = *= students;
+//console.log(students);
+
+//How to accept user input
+
+//1. Easy way = window prompt
+//2. Professonal way = HTML textbox
+
+// let username;
+// username = window.prompt("What's your username?");
+// console.log(username);
+
+// let username;
+
+// document.getElementById(`mySubmit`).onclick = function(){
+//     username = document.getElementById("myText").value;
+//     document.getElementById("myH1").textContent = `Hello ${username}`
+// }
