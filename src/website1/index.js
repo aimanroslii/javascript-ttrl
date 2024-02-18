642a821df65fb0b2acb1ea43e8606839a9cb41e5
Aiman Rosli <aimanrosli977@yahoo.com>
2024-02-18 21:27:50 +0800

    10.IF ELSE statementwith checking driving lincense simple program
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -106,3 +106,36 @@
 // Math = built-in object that provides a collection of properties and methods
 // console.log(Math.PI);
 // console.log(Math.E);
+
+//------------------
+//IF STATEMENTS = if a condition is true, execute some code; if not, do something else
+// let age = 13;
+
+// if(age >= 18){
+//     console.log("You are old enough to enter this site");
+// } else {
+//     console.log("You must be 18+ to enter this site");
+// }
+
+const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElements");
+
+mySubmit.onclick = function(){
+    age = myText.value;
+    age = Number(age);
+
+    if(age >= 100){
+        resultElement.textContent = `You are too old to enter this site.`;
+    } else if (age == 0){
+        resultElement.textContent = `You can't enter. You were just born.`;
+    } else if (age >= 18){
+        resultElement.textContent = `You are old enough to enter this site`;
+    } else if (age < 0){
+        resultElement.textContent = `Your age cant be below 0`;
+    } else {
+        resultElement.textContent = `You must be 18+ to enter this site`;
+    }
+}
+
+//------------------
