c1aa0dda7281aabbde3b03a6e644c606b7f18a07
Aiman Rosli <aimanrosli977@yahoo.com>
2024-02-18 22:06:17 +0800

    13.Switches
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -177,3 +177,35 @@
 // console.log(message);
 
 //------------------
+// SWITCH = can be an efficient replacement to many if statements
+
+// let day = 3;
+
+// switch(day){
+//     case 1:
+//         console.log("It is Monday");
+//         break;
+//     case 2:
+//         console.log("It is Tuesday");
+//         break;
+//     case 3:
+//         console.log("It is Wednesday");
+//         break;
+//     case 4:
+//         console.log("It is Thursday");
+//         break;
+//     case 5:
+//         console.log("It is Friday");
+//         break;
+//     case 6:
+//         console.log("It is Saturday");
+//         break;
+//     case 7:
+//         console.log("It is Sunday");
+//         break;
+//     default:
+//         console.log(`${day} is not a DAY`);
+//         break;
+// }
+
+//------------------
