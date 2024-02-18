99e6db2814a4b0dd32d906623e0d61bc11321dd3
Aiman Rosli <aimanrosli977@yahoo.com>
2024-02-18 20:26:15 +0800

    Update till 6.Constants
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -46,3 +46,33 @@
 //     username = document.getElementById("myText").value;
 //     document.getElementById("myH1").textContent = `Hello ${username}`
 // }
+
+//type conversion = change the datatype of a value to another 
+//                  (strngs, numbers, booleans)
+// let age = window.prompt("How old are you?");
+// age = Number(age);
+// age+=1;
+
+// console.log(age, typeof age);
+
+// let x = "pizza";
+// let y = "pizza";
+// let z = "pizza";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+// const = a variable that can't be changed
+// let pi = 3.14159;
+// let radius; 
+// let circumference;
+
+// radius = window.prompt('Enter the radius of a circle');
+// radius = Number(radius);
+// circumference = 2 * pi * radius;
+// console.log(circumference);
