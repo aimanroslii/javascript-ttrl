f1d3ff2fc182d12d6ba9b594a3322706ef2eb94d
Aiman Rosli <aimanrosli977@yahoo.com>
2024-02-18 21:02:29 +0800

    8.Math
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -80,25 +80,29 @@
 //------------
 //COUNTER PROGRAM
 
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("increaseBtn");
-const countLabel = document.getElementById("countLabel");
-let count = 0;
+// const decreaseBtn = document.getElementById("decreaseBtn");
+// const resetBtn = document.getElementById("resetBtn");
+// const increaseBtn = document.getElementById("increaseBtn");
+// const countLabel = document.getElementById("countLabel");
+// let count = 0;
 
-increaseBtn.onclick = function(){
-    count++;
-    countLabel.textContent = count;
-}
+// increaseBtn.onclick = function(){
+//     count++;
+//     countLabel.textContent = count;
+// }
 
-decreaseBtn.onclick = function(){
-    count--;
-    countLabel.textContent = count;
-}
+// decreaseBtn.onclick = function(){
+//     count--;
+//     countLabel.textContent = count;
+// }
 
-resetBtn.onclick = function(){
-    count = 0;
-    countLabel.textContent = count;
-}
+// resetBtn.onclick = function(){
+//     count = 0;
+//     countLabel.textContent = count;
+// }
 
 //-----------------
+
+// Math = built-in object that provides a collection of properties and methods
+// console.log(Math.PI);
+// console.log(Math.E);
