81f267c2e101db0b6cab1e814af7256649f9fbe0
Aiman Rosli <aimanrosli977@yahoo.com>
2024-02-18 21:55:04 +0800

    12.Ternary operator
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -141,31 +141,39 @@
 //------------------
 
 // .checked = property that determines the checked state of an HTML checkbox or radio button element
-const myCheckBox = document.getElementById("myCheckBox");
-const visaBtn = document.getElementById("visaBtn");
-const masterCardBtn = document.getElementById("masterCardBtn");
-const payPalBtn = document.getElementById("payPalBtn");
-const mySubmit = document.getElementById("mySubmit");
-const subResult = document.getElementById("subResult");
-const paymentResult = document.getElementById("paymentResult");
+// const myCheckBox = document.getElementById("myCheckBox");
+// const visaBtn = document.getElementById("visaBtn");
+// const masterCardBtn = document.getElementById("masterCardBtn");
+// const payPalBtn = document.getElementById("payPalBtn");
+// const mySubmit = document.getElementById("mySubmit");
+// const subResult = document.getElementById("subResult");
+// const paymentResult = document.getElementById("paymentResult");
 
-mySubmit.onclick = function(){
+// mySubmit.onclick = function(){
 
-    if(myCheckBox.checked){
-        subResult.textContent = `You are SUBSCRIBED !`;
-    } else {
-        subResult.textContent = `You are NOT SUBSCRIBED !`;
-    }
+//     if(myCheckBox.checked){
+//         subResult.textContent = `You are SUBSCRIBED !`;
+//     } else {
+//         subResult.textContent = `You are NOT SUBSCRIBED !`;
+//     }
 
-    if(visaBtn.checked){
-        paymentResult.textContent = `You are paying with VISA`;
-    } else if (masterCardBtn.checked){
-        paymentResult.textContent = `You are paying with MASTERCARD`;
-    } else if (payPalBtn.checked){
-        paymentResult.textContent = `You are paying with PAYPAL`;
-    } else {
-        paymentResult.textContent = `You must select a payment type !`;
-    }
-}
-
+//     if(visaBtn.checked){
+//         paymentResult.textContent = `You are paying with VISA`;
+//     } else if (masterCardBtn.checked){
+//         paymentResult.textContent = `You are paying with MASTERCARD`;
+//     } else if (payPalBtn.checked){
+//         paymentResult.textContent = `You are paying with PAYPAL`;
+//     } else {
+//         paymentResult.textContent = `You must select a payment type !`;
+//     }
+// }
 //-------------------
+//ternary operator = a shortcut to if{} else{} statements helps to assign
+//                   based on a condition 
+//                   condition ? codeIfTrue : codeIfFalse;
+// let age = 18;
+// let message = age >= 18 ? `You're an adult` : `You're a minor`;
+
+// console.log(message);
+
+//------------------
