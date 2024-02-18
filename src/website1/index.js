e990c70e168a33c62f454c8074a1edd4b9e0da2c
Aiman Rosli <aimanrosli977@yahoo.com>
2024-02-18 22:25:24 +0800

    16.Method Chaining
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -246,3 +246,9 @@
 // console.log(username);
 
 //--------------
+//Method Chaining = Calling one method after another in one continuous line of code
+let username = window.prompt("Enter your username: ");
+
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+console.log(username);
+//--------------
