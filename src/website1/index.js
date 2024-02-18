b028f4ff00fdaee5df9a4de79fa9db58923bfe78
Aiman Rosli <aimanrosli977@yahoo.com>
2024-02-18 22:49:16 +0800

    19. While loops
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -247,8 +247,18 @@
 
 //--------------
 //Method Chaining = Calling one method after another in one continuous line of code
-let username = window.prompt("Enter your username: ");
+// let username = window.prompt("Enter your username: ");
 
-username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
-console.log(username);
+// username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+// console.log(username);
+//--------------
+// WHILE LOOP
+// let username = "";
+
+// while(username === "" || username === null){
+//     username = window.prompt(`Enter your name`);
+// }
+
+// console.log(`Hello ${username}`);
+
 //--------------
