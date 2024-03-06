dcabc71460e19de8562534e5b0ed8425e02f671f
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-06 23:08:04 +0800

    Create guessing number and function
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -262,3 +262,77 @@
 // console.log(`Hello ${username}`);
 
 //--------------
+
+//NUMBER GUESSING GAME
+// const minNum = 1;
+// const maxNum = 100;
+// const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+// let attempts = 0;
+// let guess;
+// let running = true;
+
+// while(running){
+
+//     guess = window.prompt(`Guess a number betweeen ${minNum} - ${maxNum}`);
+//     guess = Number(guess);
+    
+//     if(isNaN(guess)){
+//         window.alert(`Please enter a valid number`);
+//     }
+//     else if(guess < minNum || guess > maxNum){
+//         window.alert(`Please enter a valid number`);
+//     } else {
+//         attempts++;
+//         if(guess < answer){
+//             window.alert(`TOO LOW! TRY AGAIN`);
+//         } else if (guess > answer){
+//             window.alert(`TOO HIGH! TRY AGAIN`);
+//         } else {
+//             window.alert(`CORRECT ! THE ANSWER WAS ${answer}. It took you ${attempts} attemps`);
+//             running = false;
+//         }
+//     }
+// }
+
+//-----------------
+
+//function = A section of reusable code.
+//           Declare code once, use it whenever you want 
+//           Call the function to execute that code
+
+
+
+// function happyBirthday(username, age){
+//     console.log(`Happy birthday to you!`);
+//     console.log(`Happy birthday to you!`);
+//     console.log(`Happy birthday dear ${username}!`);
+//     console.log(`Happy birthday to you!`);
+//     console.log(`You are ${age} years old`);
+// }
+
+
+// happyBirthday("BroCode", 25);
+// happyBirthday("SpongeBob", 30);
+// happyBirthday("Patrick", 37);
+
+// function add(x, y){
+//     return x + y;
+// }
+
+// function subtract(x,y){
+//     return x - y;
+// }
+
+// function multiply(x,y){
+//     return x*y;
+// }
+
+// function divide(x,y){
+//     return x/y;
+// }
+
+// console.log(add(2,3));
+
+//---------------------------------------------------------
+
