3ee0a8a93805b77b9d8f49cc7b5828d9df293758
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-09 16:55:36 +0800

    Rest parameter and spread parameter
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -380,3 +380,44 @@
 // }
 
 //----------------------------------------------------
+// spread operator = ... allows an iterable such as an array
+//                   or string to be expanded into seperate elements
+//                  (unpacks the elements)
+
+// let numbers = [1,2,3,4,5];
+// let maximum = Math.max(...numbers);
+// let minimum = Math.min(...numbers);
+
+// console.log(minimum);
+
+// let username = "Bro Code";
+// let letters = [...username];
+
+// console.log(letters);
+
+//-----------------------------
+//rest paarameters = (...rest) allow a function work with a variable number of arguments
+//                   by bundling them into an array
+//                  spread = expands an array into seperate elements
+//                  rest = bundles seperate elements into an array
+
+// const food1 = "pizza";
+// const food2 = "hamburger";
+// const food3 = "hotdog";
+// const food4 = "sushi";
+// const food5 = "ramen";
+
+// function openFridge(...foods){
+//     console.log(foods);
+// }
+
+// openFridge(food1, food2, food3, food4, food5);
+
+// function combineStrings(...strings){
+//     return strings.join(" ");
+// }
+
+// const fullName = combineStrings("Mr","Spongebob","Squarepants");
+
+// console.log(fullName);
+//-------------------------------
