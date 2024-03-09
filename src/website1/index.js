82f124e7d8a49eb59f39bbb1ff765461b4b4a38b
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-09 15:12:10 +0800

    Create Temperature Conversion Program
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -335,4 +335,29 @@
 // console.log(add(2,3));
 
 //---------------------------------------------------------
+// Temperature Conversion Program
 
+const textbox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFarenheit");
+const toCelcius = document.getElementById("toCelcius");
+const result = document.getElementById("result");
+let temp;
+
+
+function convert(){
+    if(toFahrenheit.checked){
+        temp = Number(textbox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+    } else if(toCelcius.checked){
+        temp = Number(textbox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C";
+    } else {
+        result.textContent = "Select a unit";
+    }
+
+}
+
+
+//---------------------------------------------------------
