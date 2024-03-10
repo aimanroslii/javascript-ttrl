41ec7fbd4e6a2b230b03f5c18866b8c8227abcfe
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-10 23:53:40 +0800

    callback with forEach() method
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -477,3 +477,36 @@
 // }
 
 //------------------------------
+//forEach() = method used to iterate over the elements
+//            of an array and apply a specified function (callback)
+//            to each element
+
+//            array.forEach(callback),include element, index, array
+// let numbers = [1,2,3,4,5];
+
+
+// numbers.forEach(double);
+// numbers.forEach(display);
+
+// function double(element, array, index){
+//     array[index] = element * 2;
+// }
+
+// function display(element){
+//     console.log(element);
+// }
+
+let fruits = ["apple", "orange", "banana", "coconut"];
+
+fruits.forEach(upperCase);
+fruits.forEach(display);
+
+function upperCase(element, index, array){
+    array[index] = element.toUpperCase();
+}
+
+function display(element){
+    console.log(element);
+}
+
+//------------------------------
