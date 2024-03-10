a4855d47b8b25a50cceda91c0a8941112436a4dc
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-11 00:28:05 +0800

    .reduce() method
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -566,3 +566,29 @@
 //     return element % 2 === 1;
 // }
 //--------------------------------
+// .reduce() = reduce the elements of an array to a single value
+
+// const prices = [5, 30, 10, 25, 15, 20];
+
+// const total = prices.reduce(sum);
+
+// console.log(`$${total.toFixed(2)}`);
+
+// function sum(accumulator, element){
+//     return accumulator + element;
+// }
+// or can be write as below
+
+// function sum(previous, next){
+//     return previous + next;
+// }
+
+// const grades = [75, 50, 90, 200, 65, 95];
+// const maximum = grades.reduce(getMax);
+
+// console.log(maximum);
+
+// function getMax(accumulator, element){
+//     return Math.max(accumulator,element);
+// }
+//---------------------------------
