8249b282e7e2681bc28357f8b027215386402762
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-11 00:09:55 +0800

    .filter method
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -539,13 +539,30 @@
 //     return element.toUpperCase();
 // }
 
-const dates = ["2024-1-10", "2025-2-20", "2026-3-30"];
-const formattedDates = dates.map(formatDates);
+// const dates = ["2024-1-10", "2025-2-20", "2026-3-30"];
+// const formattedDates = dates.map(formatDates);
 
-console.log(formattedDates);
+// console.log(formattedDates);
 
-function formatDates(element){
-    const parts = element.split("-");
-    return `${parts[1]}/${parts[2]}/${parts[0]}`;
-}
+// function formatDates(element){
+//     const parts = element.split("-");
+//     return `${parts[1]}/${parts[2]}/${parts[0]}`;
+// }
 //-------------------------------
+
+// .filter() = creates a new array by filtering out elements
+
+// let numbers = [1,2,3,4,5,6,7];
+// let evenNums = numbers.filter(isEven);
+// let oddNums = numbers.filter(isOdd);
+
+// console.log(oddNums);
+
+// function isEven(element){
+//     return element % 2 === 0;
+// }
+
+// function isOdd(element){
+//     return element % 2 === 1;
+// }
+//--------------------------------
