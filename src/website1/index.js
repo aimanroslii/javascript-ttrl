354b39116532e1adeb572568bbcea48bf62d945b
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-11 00:38:42 +0800

    function expressions
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -592,3 +592,32 @@
 //     return Math.max(accumulator,element);
 // }
 //---------------------------------
+// function declaration = define a reusable block of code 
+//                        that performs a specific task
+
+// function hello(){
+//     console.log("Hello");
+// }
+
+//function expressions = a way to define functions as values or variables
+
+// const hello = function(){
+//     console.log("Hello");
+// }
+
+// setTimeout(function(){
+//     console.log("Hello");
+// }, 3000);
+
+// const numbers = [1,2,3,4,5,6];
+
+// const squares = numbers.map(function(element){
+//     return Math.pow(element,2);
+// });
+
+// console.log(squares);
+
+
+//---------------------------------
+
+
