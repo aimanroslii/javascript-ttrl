dd9d17f949c9460d981a39918d9908071e28c886
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-11 00:52:54 +0800

    Arrow functions
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -617,7 +617,32 @@
 
 // console.log(squares);
 
-
 //---------------------------------
+// arrow functions = a concise way to write function expressions
+//                   good for simple functions that you use only once
+//                   (parameters) => some code
 
+//From using function declaration
+// function hello(){
+//     console.log("hello");
+// }
+// hello();
+
+//to using arrow functions
+// const hello = (name, age) => {console.log(`Hello ${name}`)
+//                               console.log(`You are ${age} years old`)};
+// hello("Bro", 23);
+
+//another example
+//setTimeout(() => console.log("Hello"), 3000);
+
+//another example
+// const numbers = [1,2,3,4,5,6];
+
+// const squares = numbers.map((element) => Math.pow(element,2));
+// const cubes = numbers.map((element) => Math.pow(element,3));
+// const evenNums = numbers.filter((element) => element % 2 === 0);
+
+// console.log(squares);
+//----------------------------------
 
