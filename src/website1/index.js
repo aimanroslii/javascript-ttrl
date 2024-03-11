609f77b332453172279c7adc9c01899a65ca9156
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-12 00:01:42 +0800

    class method with ES6 JS
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -722,3 +722,57 @@
 // console.log(car2.color);
 
 //----------------------------------
+//class = (ES6 feature) provides a more structured and cleaner way
+//        to work with objects compared to traditional constructor functions
+//        ex. static keyword, encapsulation, inheritance
+
+// function Product(name, price){
+//     this.name = name;
+//     this.price = price;
+
+//     this.displayProduct = function(){
+//         console.log(`Product: ${this.name}`);
+//         console.log(`Price: ${this.price.toFixed(2)}`);
+//     };
+
+//     this.calculateTotal = function(salesTax){
+//         return this.price + (this.price * salesTax);
+//     }
+// }
+
+// const salesTax = 0.05;
+
+// const product1 = new Product("Shirt", 19.99);
+// const product2 = new Product("Pants", 22.50);
+// const product3 = new Product("Underwear", 100.00);
+
+// product1.displayProduct();
+
+//below using class method of ES6 feature
+// class Product {
+//     constructor(name, price){
+//         this.name = name;
+//         this.price = price;
+//     }
+
+//     displayProduct(){
+//         console.log(`Product: ${this.name}`);
+//         console.log(`Price: $${this.price.toFixed(2)}`);
+//     }
+
+//     calculateTotal(salesTax){
+//         return this.price + (this.price * salesTax);
+//     }
+// }
+
+// const salesTax = 0.05;
+// const product1 = new Product("Shirt", 19.99);
+
+// product1.displayProduct();
+
+// const total = product1.calculateTotal(salesTax).toFixed(2);
+
+// console.log(total);
+
+//--------------------------------------
+
