a3e625c5d1952983c86b2247ce28ecf7c1dfba59
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-12 00:10:20 +0800

    static keyword in JS
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -775,4 +775,41 @@
 // console.log(total);
 
 //--------------------------------------
+// static = keyword that defines properties or methods that belong
+//          to a class itself rether than the objects created 
+//          from that class (class owns anything static, not the objects)
 
+// class MathUtil{
+//     static PI = 3.14159;
+
+//     static getDiameter(radius){
+//         return radius * 2;
+//     }
+
+//     static getCircumference(radius){
+//         return 2 * this.PI * radius;
+//     }
+// }
+
+
+// console.log(MathUtil.PI);
+// console.log(MathUtil.getDiameter(10));
+// console.log(MathUtil.getCircumference(10));
+
+//Example 2
+// class User{
+//     static userCount = 0;
+
+//     constructor(username){
+//         this.username = username;
+//         User.userCount++;
+//     }
+// }
+
+// const user1 = new User("Spongebob");
+// const user2 = new User("Patrick");
+
+// console.log(User.userCount);
+
+
+//--------------------------------------
