a94a22d3dfb7fe3cae44a7bc845c66f78d173d82
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-12 23:08:30 +0800

    super keyword in JS
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -849,3 +849,44 @@
 // rabbit.sleep();
 
 //---------------------------------------
+// super = keyword is used in classes to call the constructor or access
+//         the properties and methods of a parent (superclass)
+//         this = this object
+//         super = the parent
+
+// class Animal {
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }    
+    //   move(speed){
+    //     console.log(`The ${this.name} moves at a speed of ${speed}mph`);
+    //   }
+// }
+
+// class Rabbit extends Animal{
+//     constructor(name, age, runSpeed){
+//         super(name,age);
+//         this.runSpeed = runSpeed;
+//     }
+    //  run(){
+    //     console.log(`This ${this.name} can run`);
+    //     super.move(this.runSpeed);
+    //  }
+// }
+
+// class Fish extends Animal{
+//     constructor(name,age,swimSpeed){
+//         super(name,age);
+//         this.swimSpeed = swimSpeed;
+//     }
+// }
+
+
+// const rabbit = new Rabbit("rabbit", 12, 50);
+
+// console.log(rabbit.name);
+// console.log(`${rabbit.name} age : ${rabbit.age}`);
+
+
+//---------------------------------------
