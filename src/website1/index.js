90bc3df596e8707a048d4713b90223cf9fa0c87d
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-12 23:46:19 +0800

    getter and setter
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -890,3 +890,51 @@
 
 
 //---------------------------------------
+//getter = special method that makes a property readable
+//setter = special method that makes a property writeable
+
+//validate and modify a value when reading/writing a property
+
+// class Rectangle{
+//     constructor(width, height){
+//         this.width = width;
+//         this.height = height;
+//     }
+
+//     set width(newWidth){
+//         if(newWidth > 0){
+//             this._width = newWidth;
+//         } else {
+//             console.error("width must be a positive number");
+//         }
+//     }
+
+//     set height(newHeight){
+//         if(newHeight > 0){
+//             this._height = newHeight;
+//         } else {
+//             console.error("height must be a positive number");
+//         }
+//     }
+
+//     get width(){
+//         return this._width;
+//     }
+
+//     get height(){
+//         return this._height;  //'_' mean private field
+//     }
+
+//     get area(){
+//         return this._width * this._height;
+//     }
+// }
+
+// const rectangle = new Rectangle(3,4);
+
+// console.log(rectangle.width);
+// console.log(rectangle.height);
+// console.log(rectangle.area);
+
+
+//----------------------------------------
