7dcd974af0e1b5d215f2dc3eecb16d63f074d86f
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-14 22:55:18 +0800

    nested object in JS
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -1025,3 +1025,57 @@
 // displayPerson(person2);
 
 //------------------------------------------
+// nested objects = Objects inside of other Objects.
+//                  Allows you to represent more complex data structures
+//                  Child Object is enclosed by a Parent Object
+
+//                  Person{Address{}, ContactInfo{}}
+//                  ShoppingCart{Keyboard{}, Mouse{}, Monitor{}}
+
+// const person = {
+//     fullName : "Spongebob Squarepants",
+//     age : 30,
+//     isStudent : true,
+//     hobbies : ["karate", "jellyfishing", "cooking"],
+//     address : {
+//         street : "124 Conch St.",
+//         city : "Bikini Bottom",
+//         country : "Int. Water"
+//     }
+// }
+
+// console.log(person.fullName);
+// console.log(person.age);
+// console.log(person.isStudent);
+// console.log(person.hobbies);
+// console.log(person.address.street);
+
+// class Person {
+//     constructor(name, age, ...address){
+//         this.name = name;
+//         this.age = age;
+//         this.address = new Address(...address);
+//     }
+// }
+// class Address {
+
+//     constructor(street, city, country){
+//         this.street = street;
+//         this.city = city;
+//         this.country = country;
+//     }
+// }
+
+// const person1 = new Person("SpongeBob", 30,
+//                          "124 Conch St.", "Bikini Bottom", "Int waters");
+
+// const person2 = new Person("Patrick", 37,
+//                          "125 Conch St.", "Bikini Bottom", "Int waters");
+
+// const person3 = new Person("SquidWard", 32,
+//                          "126 Conch St.", "Bikini Bottom", "Int waters");
+
+// console.log(person1.address);
+
+
+//-----------
