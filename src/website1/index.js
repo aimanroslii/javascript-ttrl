da40668d5302b1cf4577d440f89e4f0a9b54697e
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-14 23:41:51 +0800

    sort method in js
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -1115,3 +1115,26 @@
 // console.log(maxFruit);
 // console.log(minFruit);
 //-----------------------------------------
+// sort() = method used to sort elements of an array in place.
+//          Sorts elements as strings in lexicographic order, not alphabetical
+//          lexicographic = (alphabet + numbers + symbols) as strings
+
+// let fruits = ["apple", "orange", "banana", "coconut", "pineapple"]
+
+// fruits.sort();
+// console.log(fruits);
+
+// let numbers = [1,3,4,10,2,5,8,7];
+// numbers.sort((a,b) => a - b);
+//for reverse order -> numbers.sort((a,b) => b - a);
+// console.log(numbers);
+
+//sort object example
+// const people = [{name : "Spongebob", age:30, gpa : 3.0},
+//                 {name : "Patrick", age: 37, gpa : 1.3}, 
+//                 {name : "Sandy", age : 32, gpa : 4.0}]
+
+// people.sort((a,b) => a.age - b.age);
+// console.log(people);
+
+//------------------------------------------
