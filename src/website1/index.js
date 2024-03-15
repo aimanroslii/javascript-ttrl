f528ac6e2cf7f7560176a360e9ac67f01749eb26
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-15 17:13:20 +0800

    element selector in JS
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -1314,3 +1314,51 @@
 //document.body.style.backgroundColor = "hsl(0,0%,15%)"; //change to darkmode by changing the background
 
 //---------------------------------------------------
+//element selectors = Methods used to target and manipulate HTML elements
+//                    They allow you to select one or multiple HTML elements
+//                    from the DOM (Document Object Model)
+
+//1. document.getElementById()           //ELEMENT OR NULL
+//2. document.getElementsClassName()     //HTML COLLECTION
+//3. document.getElementsByTagName()     //HTML COLLECTION
+//4. document.querySelector()            //ELEMENT OR NULL
+//5. document.querySelectorAll()         //NODELIST
+
+// const myHeading = document.getElementById("my-heading");
+// myHeading.style.backgroundColor = "yellow";
+// myHeading.style.textAlign = "center";
+
+// const fruits = document.getElementsByClassName("fruits");
+// console.log(fruits);
+
+// const h4Elements = document.getElementsByTagName("h4");
+// const liElements = document.getElementsByTagName("li");
+
+// console.log(h4Elements);
+// h4Elements[0].style.backgroundColor = "yellow";
+
+// for(let h4Element of h4Elements){
+//     h4Element.style.backgroundColor = "yellow";
+// }
+
+// for(let liElement of liElements){
+//     liElement.style.backgroundColor = "lightgreen";
+// }
+
+// Array.from(h4Elements).forEach(h4Element => {
+//     h4Element.style.backgroundColor = "yellow";
+// })
+
+// const element = document.querySelector(".fruits"); //only select first matching element
+// element.style.backgroundColor = "yellow";
+
+// const fruits = document.querySelectorAll(".fruits");
+// fruits[2].style.backgroundColor = "yellow";
+
+// const foods = document.querySelectorAll("li");
+
+// foods.forEach(food => {
+//     food.style.backgroundColor = "yellow";
+// })
+
+//---------------------------------------------------
