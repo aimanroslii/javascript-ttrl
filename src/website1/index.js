838fe96caeaecdfb6c0f0068e74c99ac754f426f
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-15 09:51:36 +0800

    setTimeout function in JS
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -1157,3 +1157,29 @@
 // outer();
 
 //-----------------------------------------------
+// setTimeout() = function in JavaScript that allows you to schedule
+//                the execution of a function after an amount of time(milliseconds)
+//                setTimeout(callback,delay);
+
+// function sayHello(){
+//     window.alert("Hello");
+// }
+//setTimeout(sayHello, 3000);
+
+// setTimeout(function(){window.alert("Hello")}, 3000);
+//const timeoutId = setTimeout(() => window.alert("Hello"), 3000);
+
+// let timeoutId;
+
+// function startTimer(){
+//     timeoutId = setTimeout(() => window.alert("Hello"), 3000);
+//     console.log("Starter");
+// }
+
+// function clearTimer(){
+//     clearTimeout(timeoutId);
+//     console.log("Clear");
+// }
+
+
+//-----------------------------------------------
