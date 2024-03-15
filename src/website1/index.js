5579a86bd88275e698562d0ed95f6c0827c14c14
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-15 10:34:29 +0800

    ES6 module in JS
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -1197,3 +1197,17 @@
 // setInterval(updateClock, 1000);
 
 //-------------------------------------------------
+// ES6 Module = An external file that contains reusable code
+//              that can be imported into other Javascript files.
+//              Write reusable code for many different apps.
+//              Can contain variables, classess, functions ... and more
+//              Introduced as part of ECMAScript 2015 update
+
+// import {PI, getCircumference, getArea} from './mathUtils.js';
+
+// console.log(PI);
+// const circumference = getCircumference(10);
+// console.log(`${circumference.toFixed(2)}cm`);
+
+
+//--------------------------------------------------
