4210bdcb7e81b07a96efdafb09090809ff13d470
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-15 10:53:00 +0800

    Synchronous code in callback
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -1211,3 +1211,25 @@
 
 
 //--------------------------------------------------
+// synchronous = Executes line by line consecutively in a sequential manner
+//               Code that waits for an operation to complete
+
+//asynchronous = Allows multiple opreations to be performed concurrently without waiting
+//               Doesnt block the execution flow and allows the program to continue
+//               (I/O operations, network requests, fetching data)
+//               Handled with: Callbacks, Promises, Async/Await
+
+// function func1(callback){
+//     setTimeout(()=> {console.log("Task 1");
+//                     callback()}, 3000);
+// }
+
+// function func2(){
+//     console.log("Task2");
+//     console.log("Task3");
+//     console.log("Task4");
+// }
+
+// func1(func2);
+
+//----------------------------------------------------
