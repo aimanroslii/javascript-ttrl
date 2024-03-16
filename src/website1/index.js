5d0031203cb8c6a42fc0936de9245c7187edff52
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-17 01:31:16 +0800

    JSON in js
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -1594,3 +1594,19 @@
 
 // doChores();
 //-----------------------------------------------------
+//JSON = (JavaScript Object Notation) data-interchange format
+//       Used for exchanging data between a server and a web application
+//       JSON files {key:value} OR [value1, value2, value3]
+
+//       JSON.stringify() = converts a JS object to a JSON string.
+//       JSON.parse() = converts a JSON string to a JS object
+
+// const jsonNames = `["SpongeBob", "Patrick"]`;
+// const names = ["SpongeBob", "Patrick"];
+// const jsonString = JSON.stringify(names);
+// console.log(jsonString);
+
+// const parsedData = JSON.parse(jsonNames);
+// console.log(parsedData);
+
+//-----------------------------------------------------
