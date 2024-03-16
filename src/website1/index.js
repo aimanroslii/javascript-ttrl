69af80419e182de44f17a461ec8575cd58cabf9e
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-17 01:08:20 +0800

    Promises in JS
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/website1/index.js
+++ src/website1/index.js
@@ -1463,3 +1463,63 @@
 // }
 
 //-------------------------------------------------------
+// Promise = An object that manages asynchronous operations.
+//           Wrap a Promise Object around {asynchronous code}
+//           "I promise to return value"
+//           PENDING -> RESOLVED or REJECTED
+//           new Promise((resolve, reject) => {asynchronous code})
+
+//DO THESE CHORES IN ORDER
+//1. WALK THE DOG
+//2. CLEAN THE KITCHEN
+//3. TAKE OUT THE TRASH
+
+// function walkDog(){
+
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             const dogwalked = true;
+
+//             if(dogwalked){
+//                 resolve("You walk the DOG ");
+//             } else{
+//                 reject("You DIDN'T walk the DOG");
+//             }
+//         }, 1500);
+//     })
+// }
+
+// function cleanKitchen(){
+
+//     return new Promise((resolve,reject) => {
+//         setTimeout(() => {
+//             const kitchenCleaned = true;
+
+//             if (kitchenCleaned){
+//                 resolve("You clean the KITCHEN");
+//             } else {
+//                 reject("You DIDN'T clean the KITCHEN");
+//             }
+//         }, 2500);
+//     })
+// }
+
+// function takeOutTrash(){
+
+//     return new Promise((resolve,reject) => {
+//         setTimeout(() => {
+//             const trashTakenOut = true;
+//             if(trashTakenOut){
+//                 resolve("You take out the TRASH");
+//             } else{
+//                 reject("You DIDN'T take out the TRASH");
+//             }  
+//         }, 500);
+//     })
+// }
+
+// walkDog().then(value => {console.log(value); return cleanKitchen()})
+//          .then(value => {console.log(value); return takeOutTrash()})
+//          .then(value => {console.log(value); console.log("You finished all the chores");})
+//          .catch(error => console.error(error));
+//------------------------------------------------
